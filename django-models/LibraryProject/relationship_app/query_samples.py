da44303cdd89993788a32d11a3ab32b35c839d9b
SYDNEY <lidencomputers@SYDNEYs-MacBook-Pro-4.local>
2025-07-17 11:41:27 -0400

    Changed field name from title to books in library

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -11,7 +11,7 @@ for book in books_by_author:
 library_name = "Main Library"
 try:
     library = Library.objects.get(name=library_name)
-    books_in_library = library.title.all()  # 'title' is the ManyToManyField to Book
+    books_in_library = library.books.all()  
     print(f"\nBooks in {library_name}:")
     for book in books_in_library:
         print(book.title)
