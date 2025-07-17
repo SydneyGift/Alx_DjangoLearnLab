1c1e3001cd429f2ed0bcce81d7c5993624e1c1eb
SYDNEY <lidencomputers@SYDNEYs-MacBook-Pro-4.local>
2025-07-17 11:55:29 -0400

    Changed query statements inn query samples

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,8 +1,11 @@
 from relationship_app.models import Author, Book, Library, Librarian
 
 # 1. Query all books by a specific author
-author = "Author Name"
-Author.objects.get(name=author)
+author_name = "Author Name"
+Author.objects.get(name=author_name)
+
+#OR
+author = "Authir Name"
 books_by_author = Book.objects.filter(author=author)
 
 # 2. List all books in a specific library
