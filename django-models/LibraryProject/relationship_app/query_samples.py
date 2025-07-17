3d2fb08954322d1f6c6ca0ab9e9a88bd4cc472a0
SYDNEY <lidencomputers@SYDNEYs-MacBook-Pro-4.local>
2025-07-17 11:45:46 -0400

    Changed query statements inn query samples

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -2,25 +2,16 @@ from relationship_app.models import Author, Book, Library, Librarian
 
 # 1. Query all books by a specific author
 author_name = "Author Name"
-books_by_author = Book.objects.filter(author__name=author_name)
-print(f"Books by {author_name}:")
-for book in books_by_author:
-    print(book.title)
+Author.objects.get(name=author_name)
+
 
 # 2. List all books in a specific library
 library_name = "Main Library"
-try:
-    library = Library.objects.get(name=library_name)
-    books_in_library = library.books.all()  
-    print(f"\nBooks in {library_name}:")
-    for book in books_in_library:
-        print(book.title)
-except Library.DoesNotExist:
-    print(f"No library found with name '{library_name}'")
 
+library = Library.objects.get(name=library_name)
+books_in_library = library.books.all()  
+    
 # 3. Retrieve the librarian for a specific library
-try:
-    librarian = Librarian.objects.get(library=library)
-    print(f"\nLibrarian of {library_name}: {librarian.name}")
-except Librarian.DoesNotExist:
-    print(f"No librarian assigned to library '{library_name}'")
+
+librarian = Librarian.objects.get(library=library)
+   
