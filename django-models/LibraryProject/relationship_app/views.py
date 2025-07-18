cced42141bafb2837929d849d6772ae7b80016ff
SYDNEY <lidencomputers@SYDNEYs-MacBook-Pro-6.local>
2025-07-18 18:49:14 -0400

    Implemented templates in views

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -3,6 +3,11 @@ from .models import Book
 from .models import Library
 from django.views.generic.detail import DetailView
 
+#For use with tamplates
+from django.http import HttpResponse
+from django.template import loader
+
+
 def list_books(request):
     """Retrieves all books and renders a template displaying the list."""
     books = Book.objects.all() #Fetching all book instances from the db
@@ -19,3 +24,13 @@ class LibraryDetailView(DetailView):
         context = super().get_context_data(**kwargs)
         context['book_list'] = Book.objects.filter(library=self.object)
         return context
+
+
+#Implement Templates
+def books(request):
+    template = loader.get_template('list_books.html')
+    return HttpResponse(template.render())
+
+def library_Details(request):
+    template = loader.get_template('library_detail.html')
+    return HttpResponse(template.render())
