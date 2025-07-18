af81572c245fa1c4fe6707fb97fac5c2f2358b1a
SYDNEY <lidencomputers@SYDNEYs-MacBook-Pro-6.local>
2025-07-18 16:27:21 -0400

    Changed html file path

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -11,7 +11,7 @@ def book_list(request):
 class LibraryDetailView(DetailView):
     """Displays the details of a specific library and lists all books in that library."""
     model = Library
-    template_name = 'libraries/library_detail.html'
+    template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
 
     def get_context_data(self, **kwargs):
